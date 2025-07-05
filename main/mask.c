dbd847fae2b85ae51bdbcc63cedb5fe446af3d33
MAKiT <MAKiT@horsefucker.org>
2025-07-05 02:23:48 +0200

    led test

--- main/mask.c
+++ main/mask.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <util/debug.h>
+#include <wiringPi.h>
+
+void blink (const int led)
+{
+    digitalWrite(led, HIGH);
+    delay(30);
+    digitalWrite(led, LOW);
+    delay(30);
+}
 
 void main() {
 
-    DEBUG("test");
-    DEBUG("test");
-    printf("test\n");
-    DEBUG("test");
-    DEBUG("test");
-    return;
+    wiringPiSetupGpio();
+    DEBUG("wiringSetup");
+    pinMode(2,OUTPUT);
+    DEBUG("Pin mode");
+            
+    delay(10); 
+    DEBUG("Starting loop");
+    while(1)
+    {
+	for (int j = 0; j < 4; j++)
+	{
+	    blink(leds[j]);
+	}
+    }
+    return 0;
 }
